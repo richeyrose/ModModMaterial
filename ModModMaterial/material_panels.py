16b96c5cdcf677156b9d536da7dea6131e30fdf6
Richard Rose <richard.rose@modmodterrain.com>
2022-02-10 07:33:27 +0000

    Added color ramp node

--- ModModMaterial/material_panels.py
+++ ModModMaterial/material_panels.py
@@ -33,17 +33,12 @@ class MODMODMAT_PT_Material_options(Panel):
             nodes = tree.nodes
             try:
                 parent_frame = nodes[top_level_frame]
-                display_framed_nodes(self, nodes, parent_frame)
+                create_nodes_panel(self, nodes, parent_frame)
             except KeyError:
                 pass
 
 
-def get_nodes_of_type(self, nodes, parent, type):
-    nodes = [n for n in nodes if n.parent == parent and n.type == type]
-    return sorted(nodes, key=lambda n: n.label)
-
-
-def display_framed_nodes(self, nodes, parent):
+def create_nodes_panel(self, nodes, parent):
     """Display all nodes within a frame, including nodes contained in sub frames.
 
     Args:
@@ -52,24 +47,80 @@ def display_framed_nodes(self, nodes, parent):
     """
     children = [n for n in nodes if n.parent ==
                 parent and n.type != 'FRAME']
-    frames = [n for n in nodes if n.parent == parent and n.type == 'FRAME']
-    layout = self.layout
+
+    frames = sorted([n for n in nodes if n.parent ==
+                    parent and n.type == 'FRAME'],
+                    key=lambda x: x.label)
+
     if not frames:
-        layout.label(text=parent.label)
-        for child in children:
-            layout.prop(child.outputs['Value'],
-                        'default_value', text=child.label)
+        display_framed_nodes(self, parent, children)
         return
 
     # handles nested frames
     for frame in frames:
-        display_framed_nodes(self, nodes, frame)
+        create_nodes_panel(self, nodes, frame)
 
-    # handles cases where there are both frames and nodes in a frame
     if children:
-        layout.label(text=parent.label)
-        for child in children:
-            if child.type != 'FRAME':
-                layout.prop(child.outputs['Value'],
-                            'default_value', text=child.label)
+        children = [n for n in children if n.type != 'FRAME']
+        display_framed_nodes(self, parent, children)
     return
+
+
+def display_framed_nodes(self, frame, children):
+    """Display all nodes in a frame.
+
+    Args:
+        frame (bpy.types.NodeFrame): Frame
+        children (list): List of child nodes
+    """
+    layout = self.layout
+    layout.label(text=frame.label)
+    for child in children:
+        if child.type == 'VALUE':
+            layout.prop(child.outputs['Value'],
+                        'default_value', text=child.label)
+        elif child.type == 'GROUP':
+            display_group_inputs(self, child)
+        elif child.type == 'VALTORGB':
+            display_color_ramp(self, child)
+
+
+def display_group_inputs(self, node):
+    """Display all empty inputs in a group.
+
+    Args:
+        node (bpy.types.ShaderNodeGroup): Group Node
+    """
+    inputs = [i for i in node.inputs if not i.links]
+
+    if inputs:
+        if node.label:
+            node_label = node.label
+        else:
+            node_label = node.name
+
+        layout = self.layout
+        layout.label(text=node_label)
+
+        for i in inputs:
+            if i.label:
+                input_label = i.label
+            else:
+                input_label = i.name
+            layout.prop(i, 'default_value', text=input_label)
+
+
+def display_color_ramp(self, node):
+    """Display Color Ramp nodes
+
+    Args:
+        node (bpy.types.ShaderNodeValToRGB): Color Ramp Node
+    """
+
+    if node.label:
+        node_label = node.label
+    else:
+        node_label = node.name
+    layout = self.layout
+    layout.label(text=node_label)
+    layout.template_color_ramp(node, 'color_ramp', expand=True)
